c34f929b923bd44925b51f0e9e6dcb3a455204ca
Gruesome <stefan.gruenendahl@gmail.com>
2026-07-07 08:49:58 -0700

    removed carriage returns, relocated to /opt

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 #
 # v1.0
 #
-mkdir /root/ucg-dash
-mv -v index.html chart.js server.py /root/ucg-dash/
+mkdir -p /opt/ucg-dash
+mv -v index.html chart.js server.py /opt/ucg-dash/
 mv -v ucg-dash.service.txt /etc/systemd/system/ucg-dash.service
 #
 systemctl daemon-reload
