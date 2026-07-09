3fe2e11b72aecdbb020481839c039194a518e109
grue-some <stefan.gruenendahl@gmail.com>
2026-07-08 17:03:59 -0700

    Update install.sh
    fix sed syntax

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # v1.6 :  treat current directory as install directory
 #
 cp ucg-dash.service.template ucg-dash.service
-sed -i "" "s|WORK_DIR|$(pwd)|g"  ucg-dash.service
+sed -i "s|WORK_DIR|$(pwd)|g"  ucg-dash.service
 cp -v ucg-dash.service /etc/systemd/system/ucg-dash.service
 #
 systemctl daemon-reload
