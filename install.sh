e9a0e63878b943e93d58674fec12e440d3e8866a
grue-some <stefan.gruenendahl@gmail.com>
2026-08-13 21:45:11 -0700

    Update install.sh
    chart.js and index.html in /static
    service named ucg-dash2

--- install.sh
+++ install.sh
@@ -1,13 +1,17 @@
-# install script for ucg-dash.service
+# install script for ucg-dash2.service
 #
-# v1.6 :  treat current directory as install directory
+# v3.1 :  treat current directory as install directory; 
+#         move chart.js and index.html to /static subdirectory
 #
-cp ucg-dash.service.template ucg-dash.service
-sed -i "s|WORK_DIR|$(pwd)|g"  ucg-dash.service
-cp -v ucg-dash.service /etc/systemd/system/ucg-dash.service
+mkdir -p static
+mv index.html chart.js static/
+#
+cp ucg-dash2.service.template ucg-dash2.service
+sed -i "s|WORK_DIR|$(pwd)|g"  ucg-dash2.service
+cp -v ucg-dash2.service /etc/systemd/system/ucg-dash2.service
 #
 systemctl daemon-reload
-systemctl enable ucg-dash.service
-systemctl start ucg-dash.service
-systemctl status ucg-dash.service
+systemctl enable ucg-dash2.service
+systemctl start ucg-dash2.service
+systemctl status ucg-dash2.service
 #
